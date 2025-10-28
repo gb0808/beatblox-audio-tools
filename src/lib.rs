63c73ea25b69cc30a5ed986b1d0625300fa808f9
Gabe Barnard <gbarnard08@gmail.com>
2025-10-27 23:01:20 -0500

    function name change

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use wasm_bindgen::prelude::*;
 const DEFAULT_SAMPLE_RATE: usize = 44100;
 
 #[wasm_bindgen]
-pub fn create_beatblox_oscillator(oscillator_type: &str) -> Vec<buffer::AudioBuffer> {
+pub fn create_instrument_from_oscillator(oscillator_type: &str) -> Vec<buffer::AudioBuffer> {
     let create_buffer = |i| {
         let osc = oscillator::Oscillator::new(oscillator_type, i, None);
         let raw_data: Vec<f32> = osc.take(DEFAULT_SAMPLE_RATE).collect();
